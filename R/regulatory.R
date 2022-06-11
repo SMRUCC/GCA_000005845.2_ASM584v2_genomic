c7292968f2db9a86313aa1c7b1e8202671f820e4
xieguigang <xie.guigang@gcmodeller.org>
2022-06-11 14:50:09 +0800

    export motifs?

---
+++ R/regulatory.R
@@ -0,0 +1,14 @@
+#' create all possible TFBS motifs
+#' 
+const motif_cluster = function(sites, export = "./") {
+    for(motif in sites |> read.fasta |> find_motifs(minw = 12, maxw = 16)) {
+        motif
+        |> json(compress = FALSE)
+        |> writeLines(con = `${export}/${motifString(motif)}.json`)
+        ;
+
+        bitmap(file = `${export}/${motifString(motif)}.png`) {
+            plot(motif);
+        };
+    }
+}
--- workflow/run.R
+++ workflow/run.R
@@ -3,11 +3,12 @@ imports "package_utils" from "devkit";
 # attach workflow package
 package_utils::attach(dirname(@dir));
 
-workspace = list(
-    dir = dirname(@dir)
-);
+workspace = list(dir = dirname(@dir));
+workspace$genbank_src    = `${workspace$dir}/source/GCA_000005845.2_ASM584v2_genomic.gbff`;
+workspace$upstream_sites = `${workspace$dir}/source/upstream_250bp.fasta`;
+workspace$motif_exports  = `${workspace$dir}/source/motifs/`;
 
-genbank_src = `${workspace$dir}/source/GCA_000005845.2_ASM584v2_genomic.gbff`;
-sites = extract_upstream(genbank_src, len = 200);
+sites = extract_upstream(workspace$genbank_src, len = 200);
 
-write.fasta(sites, file = `${workspace$dir}/source/upstream_250bp.fasta`, lineBreak = 80, delimiter = "|");
+write.fasta(sites, file = workspace$upstream_sites, lineBreak = 80, delimiter = "|");
+motif_cluster(workspace$upstream_sites, export = workspace$motif_exports);
