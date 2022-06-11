5875ad741cce2fcfac37ac035c8b70fbdfa8f38a
xieguigang <xie.guigang@gcmodeller.org>
2022-06-11 14:57:23 +0800

    cache data

--- workflow/run.R
+++ workflow/run.R
@@ -3,12 +3,28 @@ imports "package_utils" from "devkit";
 # attach workflow package
 package_utils::attach(dirname(@dir));
 
-workspace = list(dir = dirname(@dir));
+workspace = list(
+    dir   = dirname(@dir),
+    cache = TRUE
+);
 workspace$genbank_src    = `${workspace$dir}/source/GCA_000005845.2_ASM584v2_genomic.gbff`;
 workspace$upstream_sites = `${workspace$dir}/source/upstream_250bp.fasta`;
 workspace$motif_exports  = `${workspace$dir}/source/motifs/`;
 
-sites = extract_upstream(workspace$genbank_src, len = 200);
+const check = function(file) {
+    const cache_test = !workspace$cache;
+    const file_test  = !file.exists(file);
 
-write.fasta(sites, file = workspace$upstream_sites, lineBreak = 80, delimiter = "|");
+    cache_test || file_test; 
+}
+
+if (check(workspace$upstream_sites)) {
+    extract_upstream(workspace$genbank_src, len = 200)
+    |> write.fasta(
+        file = workspace$upstream_sites, 
+        lineBreak = 80, 
+        delimiter = "|"
+    );
+}
+
 motif_cluster(workspace$upstream_sites, export = workspace$motif_exports);
