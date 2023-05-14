8187e514f61d882388c324c2455944db82cf3d01
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 16:07:27 +0800

    algorithm test success

--- test/find_motif.R
+++ test/find_motif.R
@@ -3,6 +3,13 @@ require(GCModeller);
 imports "bioseq.fasta" from "seqtoolkit";
 imports "bioseq.patterns" from "seqtoolkit";
 
-raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
+raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\test\LexA.fasta");
 motif = find_motifs(raw, debug = TRUE);
 
+setwd(@dir);
+
+for(x in motif) {
+    bitmap(file = `${@dir}/plot/${toString(x)}.png`) {
+        plot(x);
+    }
+}
