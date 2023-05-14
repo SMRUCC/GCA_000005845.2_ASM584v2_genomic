813abfbbe78a28083e0812807d6e2b9e80f30f54
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-14 13:35:42 +0800

    config for run analysis demo script

--- workflow/run-cellular-modelling.R
+++ workflow/run-cellular-modelling.R
@@ -1,30 +1,6 @@
-imports "package_utils" from "devkit";
+require(CellRender);
 
-# attach workflow package
-package_utils::attach(dirname(@dir));
+const src_genbank = `${dirname(@dir)}/source/NC_000913.gb`;
+const cells_model = `${dirname(@dir)}/cellular_graph/`;
 
-workspace = list(
-    dir   = dirname(@dir),
-    cache = TRUE
-);
-workspace$genbank_src    = `${workspace$dir}/source/GCA_000005845.2_ASM584v2_genomic.gbff`;
-workspace$upstream_sites = `${workspace$dir}/source/upstream_250bp.fasta`;
-workspace$motif_exports  = `${workspace$dir}/source/motifs/`;
-
-const check = function(file) {
-    const cache_test = !workspace$cache;
-    const file_test  = !file.exists(file);
-
-    cache_test || file_test; 
-}
-
-if (check(workspace$upstream_sites)) {
-    extract_upstream(workspace$genbank_src, len = 200)
-    |> write.fasta(
-        file = workspace$upstream_sites, 
-        lineBreak = 80, 
-        delimiter = "|"
-    );
-}
-
-motif_cluster(workspace$upstream_sites, export = workspace$motif_exports);
+CellRender::modelling_cellgraph(src = src_genbank, outputdir = cells_model);
