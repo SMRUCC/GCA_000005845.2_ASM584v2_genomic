4df441e072ceb9b4032297fdd4e2bf0acf892e2c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-15 21:37:06 +0800

    Create find_motifs.R

---
+++ workflow/find_motifs.R
@@ -0,0 +1,11 @@
+require(GCModeller);
+
+imports "bioseq.fasta" from "seqtoolkit";
+imports "bioseq.patterns" from "seqtoolkit";
+
+setwd(@dir);
+
+raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
+pack = open.seedFile("./seeds.pack");
+motifs = find_motifs(raw, seeds = pack);
+
