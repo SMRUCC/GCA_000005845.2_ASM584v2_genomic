c22891454273d9433d6b60b909ce7062a11603bc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-16 22:08:32 +0800

    updateparameter，eters

--- workflow/find_motifs.R
+++ workflow/find_motifs.R
@@ -8,7 +8,7 @@ setwd(@dir);
 
 raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
 pack = open.seedFile("./seeds.pack");
-motifs = find_motifs(raw, seeds = pack, debug = FALSE);
+motifs = find_motifs(raw, seeds = pack, noccurs = 6, debug = FALSE);
 
 motifs
 |> JSON::json_encode()
