f8deb5e396f0bcda21fb0cd52542255a18d4ccb8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-20 00:51:30 +0800

    not working as expected

--- test/gibbs_test.R
+++ test/gibbs_test.R
@@ -2,13 +2,13 @@ require(GCModeller);
 
 imports ["bioseq.patterns", "bioseq.fasta"] from "seqtoolkit";
 
-let data = read.fasta(`${@dir}/LexA.fasta`);
-let motif = as.data.frame(gibbs_scan(data, width = 16));
+let data = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
+let motif = as.data.frame(gibbs_scan(data, width = 16, maxitr = 30));
 
 print(motif);
 
-let align = MSA.of(motif$motif);
+# let align = MSA.of(motif$motif);
 
 bitmap(file = `${@dir}/test_gibbs8.png`) {
-    plot(align);
+    plot.seqLogo(motif$motif);
 }
