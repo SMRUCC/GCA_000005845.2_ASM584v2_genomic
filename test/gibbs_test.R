47b8e295f939d152000ba1ca16bf42001bf49f39
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-19 23:23:56 +0800

    test gibbs success

--- test/gibbs_test.R
+++ test/gibbs_test.R
@@ -2,7 +2,13 @@ require(GCModeller);
 
 imports ["bioseq.patterns", "bioseq.fasta"] from "seqtoolkit";
 
-data = read.fasta(`${@dir}/LexA.fasta`);
-motif = as.data.frame(gibbs_scan(data));
+let data = read.fasta(`${@dir}/LexA.fasta`);
+let motif = as.data.frame(gibbs_scan(data, width = 16));
 
 print(motif);
+
+let align = MSA.of(motif$motif);
+
+bitmap(file = `${@dir}/test_gibbs8.png`) {
+    plot(align);
+}
