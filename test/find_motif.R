2af29a85f2ab2b1ccfd76e347ff2b95bb6002832
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-21 16:37:16 +0800

    removes old demo files

--- test/find_motif.R
+++ test/find_motif.R
@@ -1,9 +1,13 @@
+imports "package_utils" from "devkit";
+
+package_utils::attach("F:\GCModeller\src\workbench\pkg");
+
 require(GCModeller);
 
 imports "bioseq.fasta" from "seqtoolkit";
 imports "bioseq.patterns" from "seqtoolkit";
 
-raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\test\LexA.fasta");
+raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
 motif = find_motifs(raw, debug = TRUE);
 
 setwd(@dir);
