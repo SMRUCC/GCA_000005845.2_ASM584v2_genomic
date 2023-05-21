3754eb0d7500c7d044ce3391e4f1dbabcdd508e5
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-21 22:28:52 +0800

    run data filter test success

--- test/find_motif.R
+++ test/find_motif.R
@@ -8,6 +8,7 @@ imports "bioseq.fasta" from "seqtoolkit";
 imports "bioseq.patterns" from "seqtoolkit";
 
 raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\cellular_graph\tmp\.works\extract_genbank_src\upstream_locis.fasta");
+raw = read.fasta("E:\GCA_000005845.2_ASM584v2_genomic\test\LexA.fasta");
 motif = find_motifs(raw, debug = TRUE);
 
 setwd(@dir);
